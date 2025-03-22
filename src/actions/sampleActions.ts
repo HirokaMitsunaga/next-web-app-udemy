bbed5551fe5196efee6c2614c2069a959a73ee5e
hiroka <mitsunagahrk87@gmail.com>
2025-03-22 17:40:06 +0900

    SeverActions3

--- src/actions/sampleActions.ts
+++ src/actions/sampleActions.ts
@@ -1,9 +1,18 @@
 "use server";
 //サーバーアクションズのために必要
+interface FromState {
+  error: string;
+}
 
-export const createTask = async (taskId: number, formData: FormData) => {
+export const createTask = async (
+  taskId: number,
+  state: FromState,
+  formData: FormData
+) => {
   //DBにタスクを作成する
   console.log("タスクを作成しました");
   console.log(formData.get("name"));
   console.log(taskId);
+  state.error = "エラーが発生しました";
+  return state;
 };
